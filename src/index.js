db779772ca01ba7fe0fbb331639acffc2400d0b7
kierah mcgill <kierahalexis7@gmail.com>
2022-07-06 17:51:53 -0400

    commit tree 97db288b699e98c16558f6900caa908c82f1d5fd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 // write your code here
+
+
+const ramenMenu = document.getElementById("ramen-menu");
+
+fetch("http://localhost:3000")
+.then(response => response.json())
+.then(ramenObject => {
+    ramenObject.forEach(addRamen);
+})
+
+function addRamen(ramen){
+    const ramenImage = document.createElement("img");
+    ramenImage.src = ramen.image;
+
+
+
+    ramenImage.addEventListener("click", () => {
+        displayDetails(ramen);
+    })
+
+    ramenMenu.append(ramenImage);
+}
+
+function displayDetails(ramen){
+
+    const image = document.querySelector(".detail-image")
+    image.src = ramen.image;
+    
+}
